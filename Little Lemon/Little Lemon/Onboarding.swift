41b04570ba599fe644764ec72b1450e36c0fb89d
roz <rosalind.hung99@gmail.com>
2025-08-10 21:56:28 -0700

    Setting up registration, user profile, and navigation

---
+++ Little Lemon/Little Lemon/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  Little Lemon
+//
+//  Created by Roz on 8/10/25.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
---
+++ Little Lemon/Little Lemon/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  Little Lemon
+//
+//  Created by Roz on 8/10/25.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}
